c59c42d8ccb7f715ba7a255d7c4f30b52f0454c1
mdCarron <59117899+mdCarron@users.noreply.github.com>
2020-08-25 21:50:59 -0300

    Completa 4. Leyendo los datos del formulario

--- js/api.js
+++ js/api.js
@@ -10,4 +10,18 @@ class Api {
     const classifications = await responseClassifications.json();
     return classifications;
   }
+  async obtenerResultados(keyword, classification) {
+    const responseBusqueda = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/events?keyword=${keyword}&classificationId=${classification}&apikey=${this.apikey}`
+    );
+    const resultadoEventos = await responseBusqueda.json();
+    return resultadoEventos;
+  }
+  async obtenerResultadoSugerido() {
+    const resposeSugerido = await fetch(
+      `https://app.ticketmaster.com/discovery/v2/suggest?apikey=${this.apikey}`
+    );
+    const resultadoSugerido = resposeSugerido.json();
+    return resultadoSugerido;
+  }
 }
--- js/app.js
+++ js/app.js
@@ -2,3 +2,23 @@ const API = new Api("AvJAoARuWG1QQAxsQvdoexyt17vz3vMs");
 const UI = new Interfaze();
 
 UI.imprimirCategorias();
+
+document.querySelector("input#buscarBtn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const $inputBusqueda = document.querySelector("input#evento");
+  const keyword = $inputBusqueda.value;
+  const $inputCategoria = document.querySelector("select#listado-categorias");
+  const classificationId =
+    $inputCategoria.options[$inputCategoria.selectedIndex].value;
+
+  if (keyword !== "") {
+    API.obtenerResultados(keyword, classificationId).then((response) => {
+      console.log(response);
+    });
+  } else {
+    API.obtenerResultadoSugerido().then((response) => {
+      console.log(response);
+    });
+  }
+});
